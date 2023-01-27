ef504f9b70ddb2ebc75387c4bfb97ab9df001522
simycodes <simycodes@gmail.com>
2023-01-27 19:20:36 +0200

    routes route addition and cors modification

--- server.js
+++ server.js
@@ -10,10 +10,6 @@ const app = express();
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger-output.json');
 
-// http://localhost:3000/api-docs/#/
-app.use('/api-docs', swaggerUi.serve);
-app.get('/api-docs', swaggerUi.setup(swaggerDocument));
-
 // IMPORTING THE .env FILE SO ITS VALUES CAN BE ACCESSED AND USED
 require('dotenv').config();
 // SETTING THE PORT
@@ -21,6 +17,14 @@ const PORT = process.env.PORT || 3000;
 
 // VALIDATING BY PARSING INCOMING DATA-JSON FROM APP THROUGH THE BODY IN REQUEST ARGUMENT
 app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
+});
+
+// http://localhost:3000/api-docs/#/
+app.use('/api-docs', swaggerUi.serve);
+app.get('/api-docs', swaggerUi.setup(swaggerDocument));
 
 // CONNECTING TO THE DATABASE
 mongoose.set('strictQuery', true);
